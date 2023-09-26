64fd144ab1f9161d60f55fd1bcc195082ab72bd8
Ashwani Ariel <ashwani.ariel@gmail.com>
2023-09-26 18:30:44 +0530

    Add project files.

---
+++ Client/wwwroot/saveFile.js
@@ -0,0 +1,8 @@
+﻿function saveFile(fileName, fileContent) {
+    var link = document.createElement('a');
+    link.download = fileName;
+    link.href = "data:application/yaml;charset=utf-8," + encodeURIComponent(fileContent);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
---
+++ Shared/TreeNode.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeViewProject.Shared
+{
+    public class TreeNode
+    {
+        public TreeNode(int id , string nodeText , bool isSelected)
+        {
+            Id = id;
+            NodeText = nodeText;
+            IsSelected = isSelected;
+            ChildNodes = new List<TreeNode>();
+        }
+
+        public TreeNode(int id , string nodeText, bool isSelected , List<TreeNode> childNodes)
+        {
+            Id = id;
+            NodeText = nodeText;
+            IsSelected = isSelected;
+            ChildNodes = childNodes;
+        }
+
+        public int Id { get; set; }
+        public string NodeText { get; set; }
+        public List<TreeNode> ChildNodes { get; set; }
+        public bool IsSelected { get; set; } = true;
+    }
+}
